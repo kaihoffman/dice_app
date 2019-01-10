31b0e0f5a02da29a451637fe59d75b85265d7da2
Kai Hoffman <notkaiho@gmail.com>
2019-01-10 11:25:33 +0000

    rolling test created, fails

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,6 +1,13 @@
 require 'dice'
 
+# before(:each) do
+  dice = Dice.new
+  rolled = dice.roll
+# end
+
 describe Dice do
-dice = Dice.new
-it { is_expected.to respond_to :roll }
-end
+  it { is_expected.to respond_to :roll }
+  it 'returns a value between 1 & 6' do
+    expect(rolled).to be_between(1,6)
+  end
+  end
